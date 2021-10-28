d4e8668a16e58bf01627e8ee63b5ba54dfc5d09b
Javier Salcedo <javierss.2603@gmail.com>
2021-10-27 23:36:11 -0500

    clase 8: loaders para css y procesadores

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Template from './templates/Template.js';
 import './styles/main.css';
+import './styles/main2.scss';
 
 (async function App() {
   const main = null || document.getElementById('main');
